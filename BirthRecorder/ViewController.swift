749c05ca9a28673ad14a1421acc2189d1dccc49f
shajin <newuser@Shajins-MacBook-Pro.local>
2016-12-11 18:58:17 -0800

    Initial Commit

---
+++ BirthRecorder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BirthRecorder
+//
+//  Created by shajin on 12/11/16.
+//  Copyright © 2016 shajin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
